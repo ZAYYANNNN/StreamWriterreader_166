dab4e9d5955479c9234f851e7f0c0c8435761c76
ZAYYANNNN <zayyanmuhammad477@gmail.com>
2023-06-19 15:43:26 +0700

    catch default

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -11,4 +11,9 @@ int main()
 		//blok ini akan di eksekusi 
 		cout << "pengecualian ini akan di eksekusi" << endl;
 	}
+	catch (...) {
+		/*jika selain integer maka blok ini akan di eksekusi*/
+		cout << "default pengecualian di eksekusi" << endl;
+	}
+	return 0;
 }
